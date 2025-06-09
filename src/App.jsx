e41d849fe361c1fb3103139646c11989d4cdf2cb
Jeffry Hermawan <jeffryhermawantm@gmail.com>
2025-06-09 14:28:11 +0700

    fix: route merge fail app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'; 
-import Layout from './layout/AdminLayout';
-import MoviesPage from './pages/MoviesPage';
-import SchedulesPage from './pages/SchedulesPage';
-import DashboardPage from './pages/DashboardPage';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Movie_Detail from './User/Movie_Detail'; // adjust path if needed
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./layout/AdminLayout";
+import MoviesPage from "./pages/MoviesPage";
+import SchedulesPage from "./pages/SchedulesPage";
+import DashboardPage from "./pages/DashboardPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Movie_Detail from "./User/Movie_Detail"; // adjust path if needed
 import "./App.css";
-import MyTickets from './User/MyTickets';
+import MyTickets from "./User/MyTickets";
 
 // import Movie_Detail from "./User/Movie_Detail";
 function App() {
@@ -19,21 +19,14 @@ function App() {
         <Route path="/admin" element={<Layout />}>
           <Route path="dashboard" element={<DashboardPage />} />
           <Route path="movies" element={<MoviesPage />} />
+          <Route path="/movies/:id" element={<Movie_Detail />} />
           <Route path="schedules" element={<SchedulesPage />} />
+          <Route path="/history" element={<MyTickets />} />
+          {/* Add other routes here as needed */}
+          {/* Example: <Route path="/" element={<Home />} /> */}
         </Route>
-
       </Routes>
     </BrowserRouter>
-  );
-  return (
-    <Router>
-      <Routes>
-        <Route path="/movies/:id" element={<Movie_Detail />} />
-        <Route path="/history" element={<MyTickets />} />
-        {/* Add other routes here as needed */}
-        {/* Example: <Route path="/" element={<Home />} /> */}
-      </Routes>
-    </Router>
   );
 }
 
